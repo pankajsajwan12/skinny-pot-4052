c898ad5a2a2ec6921025af8894cd13e5309f630b
grvsharma1994 <03041994sharma@gmail.com>
2022-09-30 23:57:11 +0530

    day4

--- monster/src/App.js
+++ monster/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import { Footer } from './Components/Footer/Footer';
 import MainRoutes from './Components/MainRoutes';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
      <MainRoutes />
+     <Footer/>
     </div>
   );
 }
