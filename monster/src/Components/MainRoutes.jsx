ad186cb56d5ee92d0a00a0680f2c99dd65fa87fb
pankajsajwan12 <75352393+pankajsajwan12@users.noreply.github.com>
2022-10-02 00:19:35 +0530

    done

--- monster/src/Components/MainRoutes.jsx
+++ monster/src/Components/MainRoutes.jsx
@@ -5,6 +5,7 @@ import ForEmployeers from '../Page/ForEmployeers/ForEmployeers'
 import HomePage from '../Page/HomePage'
 import JobSearch from '../Page/JobSearch'
 import PremiumServices from '../Page/PremiumServices'
+import WorkDetailsPage from '../Page/WorkDetailsPage'
 import WorkFromHomes from '../Page/WorkFromHomes'
 
 const MainRoutes = () => {
@@ -13,6 +14,7 @@ const MainRoutes = () => {
         <Route path='/' element={<HomePage />}/>
         <Route path="/job-search" element={<JobSearch />}/>
         <Route path='/work-from-home-jobs' element={<WorkFromHomes />}/>
+        <Route path="/work-from-home-jobs/:id" element={<WorkDetailsPage />}/>
         <Route path='/career-advice' element={<CareerTips />}/>
         <Route path='/career-services' element={<PremiumServices />}/>
         <Route path='/for-employeers' element={<ForEmployeers />}/>
