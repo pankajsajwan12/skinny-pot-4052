0f3f2f38202497ec59723a199223d72e7deee94a
HARISH RAYALA <100848582+HarishRayala@users.noreply.github.com>
2022-10-01 12:47:52 +0530

    fw17_0700_day-5

--- monster/src/Page/ForEmployeers/ForEmployeers.jsx
+++ monster/src/Page/ForEmployeers/ForEmployeers.jsx
@@ -1,14 +1,14 @@
-import { Button, Text } from '@chakra-ui/react'
-import React from 'react'
-import Header from './Header'
-import styles from "./Header.module.css"
+import { Button, Text } from "@chakra-ui/react";
+import React from "react";
+import Header from "./Header";
+import styles from "./Header.module.css";
 
 const ForEmployeers = () => {
   return (
     <div>
-        <Header/>
-        <br />
-        {/* <div style={{display:"flex",height:"100%", border:"1px solid black"}} >
+      <Header />
+      <br />
+      {/* <div style={{display:"flex",height:"100%", border:"1px solid black"}} >
           <div>
             <img style={{height:"100%"}} src="body.png" alt="" />
           </div>
@@ -19,48 +19,137 @@ const ForEmployeers = () => {
             <button className={styles.login}>Login</button>
           </div>
         </div> */}
-        <div className={styles.employeers} >
-          <img style={{textAlign:"left",height:"400px"}} className={styles.backgroundimg} src="IT.png" alt="It" />
-          <div className={styles.body} >
-            <Text style={{display:"flex",gap:"10px"}} > <img src="icon-bg.png" alt="tickicon" /> <span>AI in Recruting Technology</span>  </Text>
-            <Text style={{display:"flex",gap:"10px"}}> <img src="icon-bg.png" alt="tickicon" /> Conversional UI</Text>
-            <Text style={{display:"flex",gap:"10px"}}> <img src="icon-bg.png" alt="tickicon" />Data Security</Text>
-            <Text style={{display:"flex",gap:"10px"}}> <img src="icon-bg.png" alt="tickicon" />Power Recruiting</Text>
-            <br />
-            <Text style={{paddingLeft:"50px"}}> Introducing</Text>
-            <Text style={{paddingLeft:"50px"}}> <b>Monster Better Together</b> </Text>
-            <Text style={{paddingLeft:"50px"}}><b>Experience</b> </Text>  
-          </div>
-          <div className={styles.employeerslogin}>
-            <b  >Login</b>
-            <br />
-            <input className={styles.employeesinput} type="email" placeholder='Username' />
-            <br />
-            <input className={styles.employeesinput} type="password" placeholder='Password' />
-            <br />
-            <br />
-            <button className={styles.loginbtn} >Login</button>
-            <br />
-            <br />
-            <hr style={{border:"0.1px solid grey"}} />
-            <br />
-            <div style={{display:"flex",margin:"auto"}} >
+      <div className={styles.employeers}>
+        <img
+          style={{ textAlign: "left", height: "400px" }}
+          className={styles.backgroundimg}
+          src="IT.png"
+          alt="It"
+        />
+        <div className={styles.body}>
+          <Text style={{ display: "flex", gap: "10px" }}>
+            {" "}
+            <img src="icon-bg.png" alt="tickicon" />{" "}
+            <span>AI in Recruting Technology</span>{" "}
+          </Text>
+          <Text style={{ display: "flex", gap: "10px" }}>
+            {" "}
+            <img src="icon-bg.png" alt="tickicon" /> Conversional UI
+          </Text>
+          <Text style={{ display: "flex", gap: "10px" }}>
+            {" "}
+            <img src="icon-bg.png" alt="tickicon" />
+            Data Security
+          </Text>
+          <Text style={{ display: "flex", gap: "10px" }}>
+            {" "}
+            <img src="icon-bg.png" alt="tickicon" />
+            Power Recruiting
+          </Text>
+          <br />
+          <Text style={{ paddingLeft: "50px" }}> Introducing</Text>
+          <Text style={{ paddingLeft: "50px" }}>
+            {" "}
+            <b>Monster Better Together</b>{" "}
+          </Text>
+          <Text style={{ paddingLeft: "50px" }}>
+            <b>Experience</b>{" "}
+          </Text>
+        </div>
+        <div className={styles.employeerslogin}>
+          <b>Login</b>
+          <br />
+          <input
+            className={styles.employeesinput}
+            type="email"
+            placeholder="Username"
+          />
+          <br />
+          <input
+            className={styles.employeesinput}
+            type="password"
+            placeholder="Password"
+          />
+          <br />
+          <br />
+          <button className={styles.loginbtn}>Login</button>
+          <br />
+          <br />
+          <hr style={{ border: "0.1px solid grey" }} />
+          <br />
+          <div style={{ display: "flex", margin: "auto" }}>
             <h5>New User?</h5>
             <a href="/">Sign up</a>
-            </div>
           </div>
         </div>
-        <div>
-            <Text fontSize="5xl"  >Our Services</Text>
-            <Text style={{fontWeight:"400"}} >For employers who need great people. Find Better, Faster.</Text>
-            <div style={{gap:"10px"}} >
-            <button className={styles.servicesbtn}>CORE SERVICES</button>
-            <button className={styles.servicesbtn}>BETTER TOGETHER</button>
-            <button className={styles.servicesbtn}>MORE SOLUTIONS</button>
-            </div>
+      </div>
+      <div>
+        <Text fontSize="5xl">Our Services</Text>
+        <Text style={{ fontWeight: "400" }}>
+          For employers who need great people. Find Better, Faster.
+        </Text>
+        <br />
+        <div className={styles.servicesbuttons}>
+          <button
+            style={{ backgroundColor: "#5d4da8", color: "white" }}
+            className={styles.servicesbtn}
+          >
+            CORE SERVICES
+          </button>
+          <button className={styles.servicesbtn}>BETTER TOGETHER</button>
+          <button className={styles.servicesbtn}>MORE SOLUTIONS</button>
+        </div>
+        <br />
+        <div className={styles.services}>
+          <div className={styles.database}>
+            <img src="resume-database-1.jpg" alt="resume-database-1.jpg" />
+            <br />
+            <b>Resume Database Access</b>
+            <br />
+            <br />
+            <Text>
+              Find your next hire with all new RDA powered by Semantic Search
+              technology that makes it easy and quick to find the talent
+              matching your requirements.
+            </Text>
+            <br />
+            <button className={styles.knowmore}>KNOW MORE</button>
           </div>
-    </div>
-  )
-}
+          <div className={styles.database}>
+            <img src="Job-Posting-1.jpg" alt="Job-Posting-1.jpg" />
+            <br />
+            <b>Job Posting</b>
+            <br />
+            <br />
 
-export default ForEmployeers
+            <Text>
+              Reach your next hire faster. Get relevant responses to your job
+              ads with our new age technology.
+            </Text>
+            <br />
+            <br />
+            <button className={styles.knowmore}>KNOW MORE</button>
+          </div>
+          <div className={styles.database}>
+            <img src="Monster-Social-1.jpg" alt="Monster-Social-1.jpg" />
+            <br />
+            <b>Monster Social Job Ads (MSJ)</b>
+            <br />
+            <br />
+
+            <Text>
+              Use the power of MSJ to take your job posting to a large pool of
+              active and passive job seekers on social media & beyond. Find
+              Better, Faster
+            </Text>
+            <br />
+            <br />
+            <button className={styles.knowmore}>KNOW MORE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForEmployeers;
